f20c1ff56557a2cee56ba049083e06c2101ebeed
Kirill <kirillk753@gmail.com>
2015-09-26 18:13:23 -0700

    Some system refactoring.

--- src/org/cs151/callrejector/schedule/Schedule.java
+++ src/org/cs151/callrejector/schedule/Schedule.java
@@ -2,33 +2,50 @@ package org.cs151.callrejector.schedule;
 
 import java.util.LinkedList;
 import java.util.List;
+import java.util.logging.Logger;
 
-import org.cs151.callrejector.schedule.exceptions.HourOutOfBoundsException;
 import org.cs151.callrejector.schedule.exceptions.InvalidTimeRangeException;
+import org.cs151.callrejector.schedule.exceptions.TimeOutOfBoundsException;
 
 /**
  * Contains and manages a collection of {@code Filter} objects.
  * @author Kirill
  */
 public class Schedule {
-	public static final int HOUR_BOUND_START = 1, HOUR_BOUND_END = 24;
+	private static final Logger log = Logger.getLogger(Filter.class.getName());
 	
 	private List<Filter> filterList = new LinkedList<>();
 	
 	public Schedule() {
-		
+		log.info("New " + Schedule.class.getName() + " instantiated successfully");
 	}
 	
 	/**
 	 * Adds a new {@code Filter} with the specified start and end times.
-	 * @param start start time of filter activity
-	 * @param end end time of filter activity
-	 * @throws HourOutOfBoundsException
-	 * @throws InvalidTimeRangeException
+	 * @param start start time of filter activity, where int[0] = hour, int[1] = minute
+	 * @param end end time of filter activity, where int[0] = hour, int[1] = minute
 	 */
-	public void addFilter(int start, int end) throws HourOutOfBoundsException, InvalidTimeRangeException {
-		filterList.add(new Filter(start, end));
+	public void addFilter(int[] start, int[] end) {
+		try {
+			addFilter(new Time(start[0], start[1]), new Time(end[0], end[1]));
+		} catch (TimeOutOfBoundsException e) {
+			log.severe(e.getMessage());
+		}
 	}
+	/**
+	 * Adds a new {@code Filter} with the specified start and end times.
+	 * @param start start time of filter activity as a {@code Time} object
+	 * @param end end time of filter activity as a {@code Time} object
+	 */
+	public void addFilter(Time start, Time end) {
+		try {
+			filterList.add(new Filter(start, end));
+		} catch (InvalidTimeRangeException e) {
+			// TODO remove this faulty filter
+			log.severe(e.getMessage());
+		}
+	}
+	
 	/**
 	 * Removes the {@code Filter} at the specified index and returns the updated filter list.
 	 * @param index index of filter to remove
--- src/org/cs151/callrejector/schedule/exceptions/InvalidTimeRangeException.java
+++ src/org/cs151/callrejector/schedule/exceptions/InvalidTimeRangeException.java
@@ -1,5 +1,7 @@
 package org.cs151.callrejector.schedule.exceptions;
 
+import org.cs151.callrejector.schedule.Time;
+
 public class InvalidTimeRangeException extends Exception {
 	private static final long serialVersionUID = 6639473009430190690L;
 	private static final String BASE_MESSAGE = "Start time must be less than end time";
@@ -7,7 +9,7 @@ public class InvalidTimeRangeException extends Exception {
 	public InvalidTimeRangeException() {
 		super(BASE_MESSAGE);
 	}
-	public InvalidTimeRangeException(int startTime, int endTime) {
-		super("Start time '" + startTime + " is greater than end time '" + endTime + "'; " + BASE_MESSAGE);
+	public InvalidTimeRangeException(Time startTime, Time endTime) {
+		super("Start time '" + startTime.displayTime() + " is greater than end time '" + endTime.displayTime() + "'; " + BASE_MESSAGE);
 	}
 }
