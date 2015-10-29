ed496988b0a985a02cbb7db43f86e8bc6715a3db
victorli5674 <victorli5674@yahoo.com>
2015-10-29 16:05:01 -0700

    Added calldeny

--- src/org/cs151/callrejector/rejector/Rejector.java
+++ src/org/cs151/callrejector/rejector/Rejector.java
@@ -1,15 +1,31 @@
-package org.cs151.callrejector.rejector;
+package com.calldeny;
 
+/*import java.lang.reflect.Method;
+  import android.app.Activity;  
+  import android.telephony.TelephonyManager;
+ import com.android.internal.telephony.*;
+import android.content.Intent;
 import android.telecom.Call;
 import android.telephony.PhoneStateListener;
+import android.view.KeyEvent;
 
 public class Rejector extends PhoneStateListener {
 
-	public void onCallStateChanged(int state, String incomingNumber)
+	public static void rejectCall() 
 	{
-		if(state == Call.STATE_RINGING)
-		{
-			
+		try{
+		    TelephonyManager tm = (TelephonyManager)context.getSystemService(Context.TELEPHONY_SERVICE);
+		    Class c = Class.forName(tm.getClass().getName());
+		    Method m = c.getDeclaredMethod("getITelephony");
+		    com.android.internal.telephony.ITelephony telephonyService = (ITelephony) m.invoke(tm);  
+		    telephonyService = (ITelephony) m.invoke(tm);
+		    telephonyService.silenceRinger();
+		    telephonyService.endCall();
+		}catch (Exception e) {
+		    e.printStackTrace();
+		      // Some problem occurred while accessing private API
+		    // TODO: do whatever error handling you want here  
+
 		}
-	}
-}
+	}*/
+
