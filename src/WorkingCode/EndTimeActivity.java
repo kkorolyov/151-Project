7a54789b9c93cbfed2c1d4dc37b9104c1ee3ba2f
Victor <victorli5674@yahoo.com>
2015-11-12 19:51:15 -0800

    Added Working code

---
+++ src/WorkingCode/EndTimeActivity.java
@@ -0,0 +1,25 @@
+package WorkingCode;
+
+import org.cs151.callrejector.gui.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+
+public class EndTimeActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		setContentView(R.layout.end_time);
+	}
+	
+	public void Cancel(View view){
+		finish();
+	}
+	
+	public void Save(View view){
+		finish();
+	}
+}
---
+++ src/WorkingCode/RejectionBlockActivity.java
@@ -0,0 +1,70 @@
+package WorkingCode;
+
+import org.cs151.callrejector.gui.R;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+/**
+ * The rejection block activity.
+ */
+public class RejectionBlockActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		setContentView(R.layout.rejection_block_activity);
+
+	}
+
+	/*
+	 * The cancel button
+	 */
+	public void CancelMakingRejectionBlock(View view) {
+		finish();
+	}
+
+	public void AddingNewRejectionBlock(View view) {
+
+		EditText New_SMS_Message = (EditText) findViewById(R.id.SMS_Message);
+
+		RejectionBlock newRejectionBlock = new RejectionBlock(
+				String.valueOf(New_SMS_Message.getText()));
+
+		Intent AddRejectionBlockToMain = new Intent();
+
+		AddRejectionBlockToMain
+				.putExtra("newRejectionBlock", newRejectionBlock);
+
+		setResult(RESULT_OK, AddRejectionBlockToMain);
+
+		finish();
+
+	}
+
+	public void RepeatDaysActivity(View view) {
+		Intent GoToRepeatDaysActivity = new Intent(this,
+				RepeatDaysActivity.class);
+
+		startActivityForResult(GoToRepeatDaysActivity, 1);
+	}
+
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+
+		Toast.makeText(this, "Edited Rejection Block", Toast.LENGTH_SHORT)
+				.show();
+	}
+	
+	public void EndTime(View view){
+		Intent GoToEndTimeActivity = new Intent(this, EndTimeActivity.class);
+		
+		startActivityForResult(GoToEndTimeActivity, 1);
+	}
+}
