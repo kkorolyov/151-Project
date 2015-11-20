fb481df9e7f8987f66706d053263180d801b9182
MarsFlight9 <MarsFlight9@users.noreply.github.com>
2015-11-19 16:08:32 -0800

    Update MissedCall.java

--- src/org/cs151/callrejector/rejector/MissedCall.java
+++ src/org/cs151/callrejector/rejector/MissedCall.java
@@ -3,7 +3,6 @@ package org.cs151.callrejector.rejector;
 import android.app.Activity;
 import android.database.Cursor;
 import android.provider.CallLog;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
