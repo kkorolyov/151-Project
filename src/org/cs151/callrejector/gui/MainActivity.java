f557d4f35ab428dc746a849bf4a2528ac1fec2d5
MarsFlight9 <chang.102@gmail.com>
2015-12-01 20:37:32 -0800

    fixed image res.

--- src/org/cs151/callrejector/gui/MainActivity.java
+++ src/org/cs151/callrejector/gui/MainActivity.java
@@ -344,6 +344,7 @@ public class MainActivity extends Activity {
         return (!this.current.isShowing() && !this.p2.isShowing()
                 &&!this.p3.isShowing() && !this.p4.isShowing()&& !this.p5.isShowing() );
     }
+    //PopUp stuff ends
 
 
 	
